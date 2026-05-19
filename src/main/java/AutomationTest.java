ac1fa0cbb0d6de65378a9cc37723c229066d6de5
Ghanshyam Mahajan <Ghanshyammali87@gmail.com>
2026-05-19 23:36:13 +0530

    Add AutomationTest Java class

---
+++ src/main/java/AutomationTest.java
@@ -0,0 +1,30 @@
+/**
+ * Automation Test Class
+ * This is a sample Java class for the Automation Project using TestNG and Cucumber
+ */
+
+public class AutomationTest {
+    
+    /**
+     * Constructor for AutomationTest class
+     */
+    public AutomationTest() {
+        System.out.println("AutomationTest class initialized");
+    }
+    
+    /**
+     * Sample test method
+     */
+    public void testSample() {
+        System.out.println("Running sample automation test");
+    }
+    
+    /**
+     * Main method for running the test
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        AutomationTest test = new AutomationTest();
+        test.testSample();
+    }
+}
